5f4fd7ed863f6a5f324353e6ffd19656fce6cdec
Liutauras.Kleniauskas <liutauras.kleniauskas@gmail.com>
2025-11-21 23:29:12 +0200

    Homework

--- features/pageobjects/side.menu.js
+++ features/pageobjects/side.menu.js
@@ -6,4 +6,17 @@ export default class SideMenu {
     get sideMenuOption() {
         return browser.$('//span[@class = "mdc-list-item__content"]//span[contains(text(),"Complaint")]');
     }
+
+    get buttonContact() {
+        return browser.$('//span[@class = "mdc-button mat-mdc-button mat-unthemed mat-mdc-button-base"]//span[contains(text(),"Contact")]');
+    }
+
+    get buttonAboutUs() {
+        return browser.$('//span[contains(text(), "About Us")]');
+    }
+
+    get versionInfoText() {
+        return browser.$('//span[contains(text(), "v18.0.0")]');
+}
+
 }
